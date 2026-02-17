50f854d3b9a3083237a48f4219f2f7e0811c840f
JamesBJqueen <jameswind47@gmail.com>
2026-02-17 08:17:50 +0100

    sum change

--- src/sections/Footer/components/FooterBottom.tsx
+++ src/sections/Footer/components/FooterBottom.tsx
@@ -2,7 +2,7 @@ export const FooterBottom = () => {
   return (
     <div className="text-gray-500 text-[13.6px] border-b-gray-500 border-l-gray-500 border-r-gray-500 border-t-slate-400/40 box-border caret-transparent leading-[21.76px] text-center mt-8 pt-[17.6px] border-t">
       <p className="box-border caret-transparent my-[13.6px]">
-        © 2025 04 Capital Investment. All rights reserved.
+        © 2026 04 Capital Investment. All rights reserved.
       </p>
       <p className="box-border caret-transparent mt-[6.4px] mb-[13.6px]">
         Business financing is subject to approval and underwriting review.
