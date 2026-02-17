1e4a463b083e2cf0db4b6a61b91d529fc32fa1ec
JamesBJqueen <jameswind47@gmail.com>
2026-02-17 09:23:36 +0100

    fix: add LoadingModal props to ApplicationForm

--- src/components/ApplicationForm.tsx
+++ src/components/ApplicationForm.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import { Navbar } from "../sections/Navbar";
 import { LoadingModal } from "../sections/Apply/Components/LoadingModal"; 
 import { Apply } from "../sections/Apply";
 import { Footer } from "../sections/Footer";
 
 export const ApplicationForm = () => {
+  const [isSubmitting, _setIsSubmitting] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleCloseError = () => {
+    setIsError(false);
+    setErrorMessage("");
+  };
+
   return (
     <>
       <Navbar/>      
-      <LoadingModal />
+      <LoadingModal 
+        isSubmitting={isSubmitting}
+        isError={isError}
+        errorMessage={errorMessage}
+        onCloseError={handleCloseError}
+      />
       <Apply/>
       <Footer/>
     </>
